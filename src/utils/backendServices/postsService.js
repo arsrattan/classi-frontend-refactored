2bd61a985db530c69a4e73ab1fc2e849139133d3
malikhadjri <mjhadjri@gmail.com>
2020-07-05 21:26:31 -0400

    liked button change

---
+++ src/utils/backendServices/postsService.js
@@ -0,0 +1,35 @@
+import {useQuery} from '@apollo/react-hooks';
+import {gql} from 'apollo-boost';
+import {useEffect, useState} from 'react';
+
+export const GetAllPosts = (userId) => {
+    const [state, setState] = useState({postsData: [], postsLoading: true});
+    const ALL_POSTS = gql`
+    query GetAllPostsForUser($userId: String!) {
+        getAllPostsForUser(userId: $userId) {
+            postId
+            postType
+            createdBy
+            createdAt
+            comments
+            likes
+            classId
+            users3url
+        }
+      }
+    `;
+    const {data, error, loading} = useQuery(ALL_POSTS, {
+        variables: {userId: userId},
+      });
+    useEffect(() => {
+      setState({postsData: [], postsLoading: true, error: null});
+      if (!error && !loading) {
+        setState({
+          postsData: data.getAllPostsForUser,
+          postsLoading: false,
+        });
+      }
+    }, [data, error, loading]);
+    return state;
+  };
+  
